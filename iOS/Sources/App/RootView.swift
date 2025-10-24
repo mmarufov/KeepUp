6a73ddae90ec98b4af854a1e85135784c66c7d7b
mmarufov <marufov254@gmail.com>
2025-10-23 22:34:28 -0700

    Add RootView component for SwiftUI application structure

---
+++ iOS/Sources/App/RootView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        FeedView(viewModel: FeedViewModel())
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
+
+
